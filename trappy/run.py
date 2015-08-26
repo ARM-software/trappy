42363c692741ca6c8b95fcbd4a45b80d8afd7e5b
Patrick Bellasi <patrick.bellasi@arm.com>
2015-08-26 15:53:21 +0100

    run: sanitise textual traces generation
    When we specify a target folder containing a trace data, the text trace is
    generated only if missing while if already present we do not check for it
    to be more recent than the corresponding binary trace.
    This could produce some not noticeable errors when running multiple time
    an experiment which override the same binary trace.
    
    This patch refactors the trace generation code to grant that a "text trace
    is always newer than the corresponding binary trace.
    
    All the conditions of the previous code are covered but one: if a text
    trace is present and newer than the binary one, we do not force the
    generation of a "raw" trace in case it is missing.
    Indeed, "raw" traces are generated contextually to "text ones, thus this
    code seems to be useful only to revamp legacy experiments.
    These old archives can still be update by simply "touching" a binary
    trace before building a run object.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- trappy/run.py
+++ trappy/run.py
@@ -106,30 +106,22 @@ classes are parsed.
             trace_name = os.path.splitext(basepath)[0]
             trace_raw = trace_name + ".raw.txt"
             trace_txt = trace_name + ".txt"
-
-            if basepath.endswith(".dat"):
-                self.__run_trace_cmd_report(basepath)
-
-            elif basepath.endswith(".txt"):
-                trace_txt = basepath
-                if not os.path.isfile(trace_raw):
-                    trace_raw = None
+            trace_dat = trace_name + ".dat"
         else:
             trace_txt = os.path.join(basepath, "trace.txt")
             trace_raw = os.path.join(basepath, "trace.raw.txt")
             trace_dat = os.path.join(basepath, "trace.dat")
 
-            if not os.path.isfile(trace_txt):
-                self.__run_trace_cmd_report(trace_dat)
+        if not os.path.isfile(trace_txt):
+            self.__run_trace_cmd_report(trace_dat)
+        elif os.path.isfile(trace_dat) and \
+                os.path.getctime(trace_txt) < os.path.getctime(trace_dat):
+            # Regenerate the "txt" file since the "dat" in the specified folder
+            # has been updated since last generation of the text trace
+            self.__run_trace_cmd_report(trace_dat)
 
-            # The condition below handles the the following cases
-            # trace.dat and trace.txt are both present
-            # We can still generate the trace.raw.txt
-            if not os.path.isfile(trace_raw):
-                if os.path.isfile(trace_dat):
-                    self.__run_trace_cmd_report(trace_dat)
-                else:
-                    trace_raw = None
+        if not os.path.isfile(trace_raw):
+            trace_raw = None
 
         return trace_txt, trace_raw
 
